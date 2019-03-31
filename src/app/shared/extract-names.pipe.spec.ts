3b013cab703e4dc6fb02b693450da4be24d50640
MANIVANNAN BABU <manivannan.b2530@gmail.com>
2019-04-01 00:54:27 +0100

    Delete extract-names.pipe.spec.ts

--- src/app/shared/extract-names.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ExtractNamesPipe } from './extract-names.pipe';
-
-describe('ExtractNamesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ExtractNamesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
